5974c223060d95ea923264471d7496e71f4d74b4
zainul-bashar <120213051+zainul-bashar@users.noreply.github.com>
2023-02-16 13:48:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 //your code here
+function convert(a){
+	a.preventDefault();
+	var inputValue=document.querySelector("input").value;
+	var arr=inputValue.split(",");
+	arr.sort(function(a,b){return a-b})
+	
+}
+var cost=0;
+while(arr.length>1){
+	var res=Number(arr[0])+Number(arr[1]);
+	arr.splice(0,2);
+	arr.push(res);
+	cost+=res;
+	arr.sort(function(a,b){return a-b});
+}
+document.getElementById("result").textContent=cost;
