fc25f2282fcd9408bba35c1707dae28df9d2207d
zainul-bashar <120213051+zainul-bashar@users.noreply.github.com>
2023-02-16 23:30:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 //your code here
-function convert(event){
+const form=document.getElementById("form");
+form.addEventListener("submit",function(event){
 	event.preventDefault();
 	var inputValue=document.querySelector("input").value;
 	var arr=inputValue.split(",");
 	arr.sort(function(a,b){
 		return a-b;
 	})
-	
-}
-var cost=0;
+	var cost=0;
 while(arr.length>1){
+	console.log(arr)
 	var res=Number(arr[0])+Number(arr[1]);
 	arr.splice(0,2);
 	arr.push(res);
@@ -18,5 +18,8 @@ while(arr.length>1){
 		return a-b;
 	});
 }
-document.getElementById("result").textContent=cost;
-document.getElementById("result").textContent=cost;
+	document.getElementById("result").textContent=cost;
+})
+
+
+// document.getElementById("result").textContent=cost;
